75036ee2f88323ff2bdf3bbaedabdf3dce428483
Amir-SaberHabibi <amirsaberhabibi80@gmail.com>
2024-08-17 18:08:11 +0330

    changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -13,7 +13,7 @@ export const EXPERIENCES = [
     title: "AI-Driven Decision Models in SE",
     company: "Uni. of Guilan",
     description: `This master’s project explores software ecosystems by analyzing common software package combinations, technologies, and code idioms. It results in a context-aware recommender system that integrates retrieval-augmented generation (RAG) with a knowledge graph design.`,
-    technologies: ["Mining Software Repos", "NLP", "Knowledge Graphs", "Software Ecosystem", "LLMs"],
+    technologies: ["Mining Software Repos", "NLP", "Knowledge Graphs", "LLMs", "Software Ecosystem"],
   },
   {
     year: "2022 - 2023",
